8a69ffd3900b0fa09dfdcf3f9d412e898574cc9c
saadscode <131830284+saadscode@users.noreply.github.com>
2023-10-16 14:45:26 -0500

    All Parts Done

--- serial.py
+++ serial.py
@@ -20,3 +20,35 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Initializes the SerialGenerator object
+
+        Args:
+            start (int): starting value of the generator
+        """
+        self.start = start
+        self.next = start
+
+    def generate(self):
+        """Generates the next number in the sequence
+
+        Returns:
+            int: generated number
+        """
+        current = self.next
+        self.next += 1
+        return current
+
+    def reset(self):
+        """Resets generator to the starting value
+        """
+        self.next = self.start
+
+    def __repr__(self):
+        """provides a string representing the SerialGenerator object
+
+        Returns:
+            str: a string representing the object
+        """
+        return f"<SerialGenerator start={self.start} next={self.next}>"
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,50 @@
-"""Word Finder: finds random words from a dictionary."""
+import doctest
+import random
 
 
 class WordFinder:
-    ...
+    def __init__(self, file_path):
+        self.words = self.read_words(file_path)
+        print(f"{len(self.words)} words read")
+
+    def read_words(self, file_path):
+        with open(file_path, 'r') as file:
+            return [line.strip() for line in file]
+
+    def random(self):
+        return random.choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    def read_words(self, file_path):
+        with open(file_path, 'r') as file:
+            return [line.strip() for line in file if line.strip() and not line.startswith('#')]
+
+
+if __name__ == "__main__":
+    wf = WordFinder("words.txt")
+
+    random_word = wf.random()
+    print(f"Random word: {random_word}")
+
+    swf = SpecialWordFinder("words.txt")
+
+    random_special_word = swf.random()
+    print(f"Random special word: {random_special_word}")
+
+
+def run_tests():
+    """
+    >>> wf = WordFinder("words.txt")
+    {} words read
+    >>> len(wf.words) == {}
+    True
+    >>> word = wf.random()
+    >>> word in wf.words
+    True
+    """.format(len(wf.words), len(wf.words))
+    pass
+
+
+if __name__ == "__main__":
+    run_tests()
